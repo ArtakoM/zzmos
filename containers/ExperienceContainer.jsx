5487060b1608d9e25cb00b5db34ca694e8b448d3
Artak Mosinyan <artakom98@gmail.com>
2020-12-10 12:21:26 +0400

    update

--- containers/ExperienceContainer.jsx
+++ containers/ExperienceContainer.jsx
@@ -3,10 +3,31 @@ import ThirdSection from '../components/ThirdSection'
 
 const experience = [
   {
+    id: 7,
+    title: 'Perigon Group',
+    vacancy: 'Lead Frontend Developer',
+    duration: '2020 Jul - Present',
+    description: 'In a Perigon Group I\'m leading front end team',
+  },
+  {
+    id: 6,
+    title: 'IQ.Academy',
+    vacancy: 'Frontend Developer',
+    duration: '2020 Sep - Present',
+    description: 'In a IQ.Academy I\'m main front end developer',
+  },
+  {
+    id: 5,
+    title: 'SVOI',
+    vacancy: 'Frontend Developer',
+    duration: '2020 Nov - Present',
+    description: 'In a SVOI CLUB I\'m main front end developer',
+  },
+  {
     id: 4,
     title: 'Polehin.com',
     vacancy: 'Web Developer',
-    duration: '2019 Jul - Present',
+    duration: '2019 Jul - 2020 May',
     description: 'Bitcoin Wallet was created in 2018 with the goal of making cryptocurrencies accessible and understandable for a wider range of users.',
   },
   {
